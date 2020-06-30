53c91a3b27dfbe4412d60570dc6711108b7ec6ee
ghostforpy <ghostformobile@gmail.com>
2020-06-30 21:14:34 +0300

    edit moex.tasks

--- moex/tasks.py
+++ moex/tasks.py
@@ -7,7 +7,6 @@ from django.utils.timezone import now
 @celery_app.task(name='moex.refresh_security')
 def refresh_security():
     today = now().date()
-    if today.weekday() not in [7, 1]:
-        securities = Security.objects.filter(last_update__lt=today)
-        for security in securities:
-            security.refresh_price()
+    securities = Security.objects.filter(last_update__lt=today)
+    for security in securities:
+        security.refresh_price()
