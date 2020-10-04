ca818db87a18c52ada34d7c9a7ebb66c70b30e48
ghostforpy <ghostformobile@gmail.com>
2020-10-04 22:47:02 +0300

    add TradeHistoryViewSet action destroy

--- moex/api/views.py
+++ moex/api/views.py
@@ -2,16 +2,16 @@
 # from django.shortcuts import get_object_or_404
 # from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin,\
-    RetrieveModelMixin
-#    UpdateModelMixin
-# from rest_framework.response import Response
+    RetrieveModelMixin, DestroyModelMixin
+from rest_framework import status as response_status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated,\
     AllowAny
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.pagination import PageNumberPagination
 from .serializers import SecurityRetrivieSerializer,\
-    SecurityListSerializer
-from ..models import Security
+    SecurityListSerializer, TradeHistorySerializer
+from ..models import Security, TradeHistory
 
 
 class PageNumberPaginationBy5(PageNumberPagination):
@@ -43,3 +43,29 @@ class SecurityViewSet(ListModelMixin,
         else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
+
+
+class TradeHistoryViewSet(  # DestroyModelMixin,
+        GenericViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'destroy':
+            return TradeHistorySerializer
+
+    def get_queryset(self):
+        queryset = TradeHistory.objects.all()
+        user = self.request.user
+        return queryset.filter(owner=user)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        s = self.perform_destroy(instance)
+        if s == 'ok':
+            status = response_status.HTTP_204_NO_CONTENT
+        else:
+            status = response_status.HTTP_400_BAD_REQUEST
+        return Response(data=s, status=status)
+
+    def perform_destroy(self, instance):
+        return instance.delete()
