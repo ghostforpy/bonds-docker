589d82808669c368bf95c6d0768f61d6b86c479f
ghostforpy <ghostformobile@gmail.com>
2020-10-04 22:46:37 +0300

    add router for securities-trade-history

--- moex/api/urls.py
+++ moex/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from django.urls import path, include
-from moex.api.views import SecurityViewSet
+from moex.api.views import SecurityViewSet, TradeHistoryViewSet
 
 
 if settings.DEBUG:
@@ -10,6 +10,10 @@ else:
     router = SimpleRouter()
 
 router.register('securities', SecurityViewSet)
+router.register('securities-trade-history',
+                TradeHistoryViewSet,
+                basename='securities-trade-history')
+# include delete method
 
 urlpatterns = [
     path('', include(router.urls)),
