edd2643df7311f753b8c1a61fc69e896c65b036d
ghostforpy <ghostformobile@gmail.com>
2020-10-27 14:26:16 +0300

    refuctor

--- bonds/static/js/follow.js
+++ bonds/static/js/follow.js
@@ -1,19 +1,19 @@
-$(document).on('click', '#follow', function(e){
+$(document).on('click', '#follow', function (e) {
     e.preventDefault();
     let elem = $(this);
     $.post(
         $(this).attr('href'),
         {},
-        function(data){
-            if (data['status'] == 'ok'){
+        function (data) {
+            if (data['status'] == 'ok') {
                 elem.toggleClass('btn-danger');
                 elem.toggleClass('btn-primary');
                 if (data['result'] == 'added') {
                     elem.text('Отписаться')
-                }else{
+                } else {
                     elem.text('Подписаться')
                 };
             };
         }
     );
-    });
+});
