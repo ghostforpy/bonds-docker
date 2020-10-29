504943dd3534111a8efdf96546c834de2bf36e41
ghostforpy <ghostformobile@gmail.com>
2020-10-29 20:07:18 +0300

    first config app breports

--- breports/admin.py
+++ breports/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import BReport
 # Register your models here.
+
+
+@admin.register(BReport)
+class BReportAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner', 'filename', 'created']
+    list_filter = ('owner',)
--- breports/models.py
+++ breports/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/broker_reports/user_<id>/<filename>
+    return 'broker_reports/user_{0}/{1}'.format(instance.owner.id, filename)
+
+
+class BReport(models.Model):
+    # model for upload broker reports
+    owner = models.ForeignKey(
+        User,
+        related_name='breports',
+        verbose_name='owner',
+        on_delete=models.CASCADE
+    )
+    filename = models.FileField(upload_to=user_directory_path)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.filename)
