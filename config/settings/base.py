487908eef465b0a2d07d3910b7f709f702fe2378
ghostforpy <ghostformobile@gmail.com>
2020-11-01 20:55:01 +0300

    change rest authentication

--- config/settings/base.py
+++ config/settings/base.py
@@ -302,8 +302,8 @@ SOCIALACCOUNT_ADAPTER = "bonds.users.adapters.SocialAccountAdapter"
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
