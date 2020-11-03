391500c00ff29d2ff6037ab69941403bffd4689d
ghostforpy <ghostformobile@gmail.com>
2020-11-03 21:28:33 +0300

    edit main.js breport frontend

--- bonds/static/vue/breports/main.js
+++ bonds/static/vue/breports/main.js
@@ -5,10 +5,10 @@ var app = new Vue({
     data: {
         message: 'Давайте проанализируем Ваши брокерские отчеты!',
         file: null,
-        year_profit_visible: false,
         spiner_visible: false,
         errors: null,
         errors_visible: false,
+        year_profit_visible: false,
         year_profit_data: null
     },
     methods: {
@@ -28,14 +28,33 @@ var app = new Vue({
                     console.log('SUCCESS!!');
                     em.year_profit_data = resp.data;
                     em.year_profit_visible = true;
-                    console.log(resp);
+                    //console.log(resp);
                 })
-                    .catch(function (e) {
+                    .catch(function (error) {
                         em.spiner_visible = false;
                         em.errors_visible = true;
                         console.log('FAILURE!!');
-                        em.errors = e.response.data;
-                        console.log(error);
+                        if (error.response) {
+                            // The request was made and the server responded with a status code
+                            // that falls out of the range of 2xx
+                            //console.log(error.response.data);
+                            if (error.response.status === 500) {
+                                em.errors = ['Server error'];
+                            } else {
+                                em.errors = error.response.data;
+                            }
+                            //console.log(error.response.status);
+                            //console.log(error.response.headers);
+                        } else if (error.request) {
+                            // The request was made but no response was received
+                            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                            // http.ClientRequest in node.js
+                            //console.log(error.request);
+                        } else {
+                            // Something happened in setting up the request that triggered an Error
+                            //console.log('Error', error.message);
+                        }
+                        //console.log(error.config);
                     });
             }
         },
