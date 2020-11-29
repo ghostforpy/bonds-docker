ce8cef27475d3a56449b50994abc81f0ccace79b
ghostforpy <ghostformobile@gmail.com>
2020-11-29 22:38:11 +0300

    add ToogleEnableMixin to informer

--- informer/admin.py
+++ informer/admin.py
@@ -3,7 +3,20 @@ from .models import UserInformer
 # Register your models here.
 
 
+class ToogleEnableMixin():
+    def toogle_enable(self, request, queryset):
+        for obj in queryset:
+            if obj.enable:
+                obj.enable = False
+            else:
+                obj.enable = True
+            obj.save()
+
+    toogle_enable.short_description = "Отключить/Включить"
+    actions = [toogle_enable]
+
+
 @admin.register(UserInformer)
-class UserInformerAdmin(admin.ModelAdmin):
+class UserInformerAdmin(ToogleEnableMixin, admin.ModelAdmin):
     list_display = ['id', 'user', 'enable']
     list_filter = ('user', 'enable',)
