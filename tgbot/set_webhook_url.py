89d753bfbf8f1c8a0c58975d873a20d8e7625ebb
ghostforpy <ghostformobile@gmail.com>
2021-05-26 23:03:50 +0300

    set commands util

--- tgbot/set_webhook_url.py
+++ tgbot/set_webhook_url.py
@@ -1,18 +1,36 @@
 import os
+import json
 from sys import argv
 import requests
-from django.urls import reverse
 
+from django.urls import reverse
+import available_commands
+import sys
+
+
+AVAILABLE_COMMANDS = available_commands.AVAILABLE_COMMANDS
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", False)
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", False)
-print('Set telegram webhook url')
+
 if TELEGRAM_BOT_TOKEN and WEBHOOK_URL:
+    print('Set telegram webhook url')
 
     url = 'https://api.telegram.org/bot{}/setWebhook?url={}/tgwebhook/'.format(
         TELEGRAM_BOT_TOKEN,
         WEBHOOK_URL
     )
     r = requests.post(url)
+    json_response = r.json()
+    print(json_response)
+
+    print('Set telegram bot commands')
+    url = 'https://api.telegram.org/bot{}/setMyCommands'.format(
+        TELEGRAM_BOT_TOKEN
+    )
+    commands = json.dumps([
+        {'command': i, 'description': AVAILABLE_COMMANDS[i]} for i in AVAILABLE_COMMANDS
+    ])
+    r = requests.post(url, data={'commands': commands})
     json_response = r.json()
     print(json_response)
 else:
