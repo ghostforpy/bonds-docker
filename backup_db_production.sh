368e8f88c6ba4fc7e42df9f00a8cada2b3bb5b08
ghostforpy <ghostformobile@gmail.com>
2022-02-27 10:01:22 +0300

    fix

--- backup_db_production.sh
+++ backup_db_production.sh
@@ -4,4 +4,4 @@ IFS=' ' read -ra my_array <<< "$t"
 docker_id=$my_array
 IFS="'" read -ra my_array2 <<< `sudo docker-compose -f production.yml exec postgres backup | grep SUCCESS`
 backup_id=${my_array2[3]}
-sudo docker cp $docker_id:/backups/$backup_id ../backups/
+sudo docker cp $docker_id:/backups/$backup_id ../../backups/
