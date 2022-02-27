5b1f56979e05d8eb404346b82cbab3fe47968b61
ghostforpy <ghostformobile@gmail.com>
2022-02-27 09:45:40 +0300

    fix html breports app

--- breports/views.py
+++ breports/views.py
@@ -7,5 +7,5 @@ def main(request):
     mode = os.environ['DJANGO_SETTINGS_MODULE']
     mode = mode.split('.')[-1]
     return render(request,
-                  'breports/main.html',
+                  'breports/main_with_vue.html',
                   {'mode': mode})
